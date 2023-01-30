ea132bbb73dd6a9225d2e758bbefe013334bf815
loginoff-vg <loginoff74@gmail.com>
2023-01-31 05:12:20 +0700

    решение задания: 4.Объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,26 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.age = age;
+  this.gender = gender;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName || 'javaScript';
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (Array.isArray(this.marks)) {
+    this.marks = [...this.marks, ...marks];
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  return ( !Array.isArray(this.marks) || !this.marks.length ) ? 0 : this.marks.reduce((total, mark)=>total+=mark, 0)/this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+  delete this.marks;
+  delete this.subject;
 }
